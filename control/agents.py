369b06bfc0f3e651f948e0a235a71942ad3680a2
Basile Dura <basile.dura@polytechnique.edu>
2019-04-05 00:54:35 -0400

    Added gradient clipping to avoid exploding gradients.

--- control/agents.py
+++ control/agents.py
@@ -101,6 +101,9 @@ class DQNAgent(BaseAgent):
         loss = self.criterion(q, torch.tensor(target))
         loss.backward(retain_graph=True)
 
+        torch.nn.utils.clip_grad_norm_(self.model.parameters(), 1.0)
+        torch.nn.utils.clip_grad_value_(self.model.parameters(), 0.5)
+
         self.optimiser.step()
 
     def reset(self):
